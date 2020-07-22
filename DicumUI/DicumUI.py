d8fd74ab7b116559336fcc563bececca81d970a2
Jonas Kalinka <jonas.kalinka@web.de>
2020-07-22 23:13:31 +0200

    added html viewer

--- DicumUI/DicumUI.py
+++ DicumUI/DicumUI.py
@@ -1,5 +1,38 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QMainWindow, QVBoxLayout
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QMainWindow, QVBoxLayout, QWidget
+from PyQt5.QtGui import QPixmap
+from PyQt5 import QtWebEngineWidgets
+from PyQt5 import QtCore
+
+class ButtonWidget(QWidget):
+	def __init__(self, *args, **kwargs):
+		super(QWidget, self).__init__(*args, **kwargs)
+
+		self.counter = 0
+
+		self.label = QLabel(str(self.counter))
+		button = QPushButton("Click Me!")
+		button.clicked.connect(lambda:self.count())
+
+		layout = QVBoxLayout()
+		frame = QPixmap("resources/chick.jpeg")
+
+		layout.addWidget(self.label)
+		imageLabel = QLabel(self)
+		imageLabel.setPixmap(frame)
+		layout.addWidget(imageLabel)
+		layout.addWidget(button)
+
+		view = QtWebEngineWidgets.QWebEngineView()
+		view.load(QtCore.QUrl().fromLocalFile("/home/jonas/Projects/dicum/resources/test.html"))
+		layout.addWidget(view)
+		self.setLayout(layout)
+
+	def count(self):
+		self.counter += 1
+		self.label.setText(str(self.counter))
+
+	def PrintHello(self):
+		print("Hello")
 
 
 class MainWindow(QMainWindow):
@@ -7,9 +40,8 @@ class MainWindow(QMainWindow):
 		super(MainWindow, self).__init__(*args, **kwargs)
 
 		self.setWindowTitle("Dicum")
-		label = QLabel("Hello, World!")
-		label.setAlignment(Qt.AlignCenter)
-		self.setCentralWidget(label)
+		mainWidget = ButtonWidget(self)
+		self.setCentralWidget(mainWidget)
 
 
 if __name__ == '__main__':
