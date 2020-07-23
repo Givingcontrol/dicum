6341824b94609928dd2ea7814bdcabf18a4d742d
Jonas Kalinka <jonas.kalinka@web.de>
2020-07-23 18:40:35 +0200

    basic button functions implemented

--- DicumUI/ContentGenerator.py
+++ DicumUI/ContentGenerator.py
@@ -7,10 +7,18 @@ class ContentGenerator:
 		file = open("resources/commands.txt", "r")
 		lines = file.readlines()
 		file.close()
-		for i,x in enumerate(lines):
+		for i, x in enumerate(lines):
 			if i < len(self.content):
 				self.content[i] = x
 		random.shuffle(self.content)
 
-	def getNext(self):
-		return self.content.pop()
+	def get_next(self):
+		try:
+			return self.content.pop()
+		except:
+			return -1
+
+
+
+# TODO 5 min viewing time for web page
+# TODO Chose 3 of 5 Cards with Pictures (Cens/NoCens)
