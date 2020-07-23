2fe749eb54672363f93859a83e879a6bd0af955f
Jonas Kalinka <jonas.kalinka@web.de>
2020-07-24 00:02:30 +0200

    html can be loaded except for images, basic kinds implemented

--- DicumUI/ContentGenerator.py
+++ DicumUI/ContentGenerator.py
@@ -1,24 +1,55 @@
+import csv
 import random
 
 
 class ContentGenerator:
 	def __init__(self, size):
-		self.content = [i for i in range(size)]
-		file = open("resources/commands.txt", "r")
-		lines = file.readlines()
-		file.close()
-		for i, x in enumerate(lines):
-			if i < len(self.content):
-				self.content[i] = x
-		random.shuffle(self.content)
+		self.commands = ContentGenerator.get_commands(size)
 
 	def get_next(self):
 		try:
-			return self.content.pop()
+			return self.commands.pop()
 		except:
+			print("get_next failed, no more elements to pop")
 			return -1
 
+	@staticmethod
+	def get_html_base_front():
+		html_template = open("resources/test.html")
+		html_base = "".join(html_template.readlines())
+		html_template.close()
+		return html_base
 
+	@staticmethod
+	def get_html_image(path):
+		return '<center><img src="' + path + '" align="bottom"/> </center>'
+
+	@staticmethod
+	def get_html_back():
+		return "</BODY></HTML>"
+
+	@staticmethod
+	def interpret_command(command):
+		text, kind, time = command
+		if kind == "img":
+			return "html", ContentGenerator.get_html_base_front() + ContentGenerator.get_html_image(
+				text) + ContentGenerator.get_html_back()
+		elif kind == "url":
+			return "url", text
+		else:
+			print("Command kind could not be interpreted:", kind, "Full:", command)
+
+	@staticmethod
+	def get_commands(size):
+		commands = [("num", str(i)) for i in range(size)]
+		with open("resources/commands.csv") as csvfile:
+			reader = csv.reader(csvfile, delimiter=",")
+			for i, row in enumerate(reader):
+				if i < len(commands):
+					commands[i] = ContentGenerator.interpret_command(row)
+
+		random.shuffle(commands)
+		return commands
 
 # TODO 5 min viewing time for web page
 # TODO Chose 3 of 5 Cards with Pictures (Cens/NoCens)
--- DicumUI/DicumUI.py
+++ DicumUI/DicumUI.py
@@ -1,58 +1,60 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QMainWindow, QVBoxLayout, QWidget, QGridLayout
-from PyQt5.QtGui import QPixmap
-from PyQt5 import QtWebEngineWidgets
-from PyQt5 import QtCore
-from ContentGenerator import ContentGenerator
-from threading import Timer
 from functools import partial
+
+from ContentGenerator import ContentGenerator
+from PyQt5 import QtCore
+from PyQt5 import QtWebEngineWidgets
+from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, QVBoxLayout, QWidget, QGridLayout
 
 
 class ButtonWidget(QWidget):
 	def __init__(self, *args, **kwargs):
 		super(QWidget, self).__init__(*args, **kwargs)
 
-		self.generator = ContentGenerator(12)
-
-		button_cols = 4
-		button_rows = 3
-		self.button_array = [QPushButton("x") for i in range(button_cols * button_rows)]
+		self.button_cols = 4
+		self.button_rows = 3
+		self.generator = ContentGenerator(self.button_cols * self.button_rows)
+		self.button_array = [QPushButton("x") for i in range(self.button_cols * self.button_rows)]
 
 		self.button_widget = QWidget(self)
 		button_layout = QGridLayout()
-		for row in range(button_rows):
-			for col in range(button_cols):
-				pos = row * button_cols + col
+		for row in range(self.button_rows):
+			for col in range(self.button_cols):
+				pos = row * self.button_cols + col
 				button_layout.addWidget(self.button_array[pos], row, col)
 				self.button_array[pos].clicked.connect(partial(self.get_content, pos))
-		self.button_widget.setLayout(button_layout)
 
-		self.label = QLabel("-1")
+		self.button_widget.setLayout(button_layout)
 
 		layout = QVBoxLayout()
 
-		# add image view
-		# frame = QPixmap("resources/chick.jpeg")
-		# image_label = QLabel(self)
-		# image_label.setPixmap(frame)
-		# layout.addWidget(image_label)
-
-		view = QtWebEngineWidgets.QWebEngineView()
-		#view.load(QtCore.QUrl().fromLocalFile("/home/jonas/Projects/dicum/resources/test.html"))
-		view.load(QtCore.QUrl().fromLocalFile("/home/jonas/Projects/dicum/resources/chick.jpeg"))
-		#view.load(QtCore.QUrl("https://google.com"))
-		layout.addWidget(view)
+		self.view = QtWebEngineWidgets.QWebEngineView()
+		# view.load(QtCore.QUrl().fromLocalFile("/home/jonas/Projects/dicum/resources/test.html"))
+		# view.load(QtCore.QUrl().fromLocalFile("/home/jonas/Projects/resources/chick.jpeg"))
+		self.view.setHtml("Welcome to Dicum")
+		# view.load(QtCore.QUrl("https://google.com"))
+		layout.addWidget(self.view)
 		layout.addWidget(self.button_widget)
-		layout.addWidget(self.label)
 		self.setLayout(layout)
 
 	def get_content(self, pos):
+		kind = ""
+		content = ""
 		try:
 			self.button_array[pos].setEnabled(False)
-			self.label.setText(str(self.generator.get_next()))
-		except:
+			kind, content = self.generator.get_next()
+		except IndexError:
 			print("Array position", pos, "is out of range")
 
-	def print_hello(self):
+		print(content)
+		if kind == "html" or kind == "num":
+			self.view.setHtml(content)
+		elif kind == "url":
+			self.view.load(QtCore.QUrl(content))
+		else:
+			print("content kind was not recognized:", kind, content)
+
+	@staticmethod
+	def print_hello():
 		print("Hello World")
 
 
