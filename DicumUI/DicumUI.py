66a43cc4fd262cf3f3af160f27c4902e4313bdcb
Jonas Kalinka <jonas.kalinka@web.de>
2020-08-05 22:55:04 +0200

    style changes and minor stuff

--- DicumUI/DicumUI.py
+++ DicumUI/DicumUI.py
@@ -1,18 +1,17 @@
 import glob
-import os
+import os, sys
 from functools import partial
 
 from ContentGenerator import ContentGenerator
 from PyQt5 import QtCore
 from PyQt5 import QtWebEngineWidgets
-from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, QVBoxLayout, QWidget, QGridLayout
+from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, QVBoxLayout, QWidget, QGridLayout, QSizePolicy
+from PyQt5 import QtWidgets
 
 
-class ButtonWidget(QWidget):
+class MainGameWidget(QWidget):
 	def __init__(self, *args, **kwargs):
 		super(QWidget, self).__init__(*args, **kwargs)
-
-		reset = QPushButton("Restart")
 
 		self.button_cols = 4
 		self.button_rows = 3
@@ -20,23 +19,24 @@ class ButtonWidget(QWidget):
 		self.button_array = [QPushButton("x") for i in range(self.button_cols * self.button_rows)]
 
 		self.button_widget = QWidget(self)
-		button_layout = QGridLayout()
+		self.button_widget.setFixedSize(500, 150)
+		self.button_widget.setLayout(QGridLayout())
 		for row in range(self.button_rows):
 			for col in range(self.button_cols):
 				pos = row * self.button_cols + col
-				button_layout.addWidget(self.button_array[pos], row, col)
+				self.button_widget.layout().addWidget(self.button_array[pos], row, col)
 				self.button_array[pos].clicked.connect(partial(self.get_content, pos))
 
-		self.button_widget.setLayout(button_layout)
+		self.button_widget.setSizePolicy(
+			QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.MinimumExpanding))
 
-		layout = QVBoxLayout()
-
+		# Setting up the view
 		self.view = QtWebEngineWidgets.QWebEngineView()
-		self.view.setHtml("Welcome to Dicum")
+		self.view.setHtml('<center style="margin-top:7em">Welcome to Dicum</center>')
 
-		layout.addWidget(self.view)
-		layout.addWidget(self.button_widget)
-		self.setLayout(layout)
+		self.setLayout(QVBoxLayout())
+		self.layout().addWidget(self.view)
+		self.layout().addWidget(self.button_widget)
 
 	def get_content(self, pos):
 		self.button_array[pos].setEnabled(False)
@@ -54,30 +54,30 @@ class ButtonWidget(QWidget):
 			print("content kind was not recognized:", kind, content)
 
 
-@staticmethod
-def print_hello():
-	print("Hello World")
-
-
 class MainWindow(QMainWindow):
 	def __init__(self, *args, **kwargs):
 		super(MainWindow, self).__init__(*args, **kwargs)
 
 		self.setWindowTitle("Dicum")
-		main_widget = ButtonWidget(self)
+		self.menuBar().addMenu("Dicum")
+		main_widget = MainGameWidget(self)
 		self.setCentralWidget(main_widget)
+		self.setGeometry(0, 0, 1500, 1150)
+
+		self.show()
 
 
 if __name__ == '__main__':
 	app = QApplication([])
-	app.setStyleSheet("QPushButton { background-color: blue }")
+	app.setStyleSheet(
+		"QPushButton { background-color: darkred; color: white } QMainWindow { background-color: black }")
 
 	main_window = MainWindow()
-	main_window.show()
-
 	app.exec()
 
 	# clear temp folder
 	files = glob.glob('temp/*')
 	for f in files:
 		os.remove(f)
+
+	sys.exit()
