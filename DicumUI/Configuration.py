10a4ba29c1feb5172ac9c8566407be3a803a2a05
joka42 <jas.nombre@web.de>
2020-08-16 10:36:25 +0200

    os path changed to var/lib/dicum

--- DicumUI/Configuration.py
+++ DicumUI/Configuration.py
@@ -4,19 +4,18 @@ import os
 class Configuration:
 	class __Configuration:
 		def __init__(self):
+			if os.name != "posix":
+				print("Only linux systems are currently supported.")
+				exit(1)
 			self.LOCK_LIMIT = 2
 			self.UNLOCK_LIMIT = 2
 			self.TIME_FORMAT = "%y-%m-%d %H:%M:%S"
-
-			if os.name != "posix":
-				print("Currently, only linux systems are supported.")
-				exit(1)
-
 			self.TEMP_LOCATION = "/tmp/.dicum"
 			self.TEMP_IMAGES = os.path.join(self.TEMP_LOCATION, "images")
 			self.TEMP_SCRIPTS = os.path.join(self.TEMP_LOCATION, "js")
-			self.LOCK_TIME_LOCATION = "/var/dicum/dicum"
+			self.LOCK_TIME_LOCATION = "/var/lib/dicum/dicum"
 			self.HTML_REL_PATH = "file://" + self.TEMP_LOCATION + "/"
+			self.BG_IMAGE = "images/bg03.png"
 			try:
 				os.makedirs(self.TEMP_LOCATION)
 				os.makedirs(self.TEMP_IMAGES)
